6d4592e9d3d87d7babc6ba9113333cef0c7dbae0
DMuravskij <DMuravskij@mtsbank.ru>
2021-03-29 11:58:10 +0300

    add methods for UserHolder.kt, add method and constructor for User.kt

--- app/src/main/java/ru/skillbranch/kotlinexample/User.kt
+++ app/src/main/java/ru/skillbranch/kotlinexample/User.kt
@@ -68,6 +68,20 @@ class User private constructor(
         sendAccessCodeToUser(rawPhone, code)
     }
 
+    //for CSV
+    constructor(
+            firstName: String,
+            lastName: String?,
+            email: String?,
+            salt: String,
+            password: String,
+            rawPhone: String
+    ): this(firstName, lastName, email = email, rawPhone = rawPhone, meta = mapOf("src" to "csv")) {
+        println("Secondary CSV constructor")
+        this.salt = salt
+        this.passwordHash = password
+    }
+
     init {
         println("First init block, primary constructor was called")
 
@@ -101,11 +115,11 @@ class User private constructor(
         } else throw IllegalArgumentException("The entered password does not match the current password")
     }
 
-    private fun sendAccessCodeToUser(phone: String?, code: String) {
+    fun sendAccessCodeToUser(phone: String?, code: String) {
         println(".... sending access code: $code on $phone")
     }
 
-    private fun generateAccessCode(): String {
+    fun generateAccessCode(): String {
         val possible = "ABCDEFGabcdefgZJEEzjee0123456789"
 
         return StringBuilder().apply {
@@ -164,5 +178,17 @@ class User private constructor(
                                         "current split result: ${this@fullNameToPair}  ")
                             }
                         }
+
+        fun importUser(rawUser: String): User {
+            val userInfo = rawUser.split(";", ":")
+            val(firstName, lastName) = userInfo[0].fullNameToPair()
+            return User(firstName,
+                        lastName,
+                        userInfo[1],
+                        userInfo[2],
+                        userInfo[3],
+                        userInfo[4]
+            )
+        }
     }
 }
--- app/src/main/java/ru/skillbranch/kotlinexample/UserHolder.kt
+++ app/src/main/java/ru/skillbranch/kotlinexample/UserHolder.kt
@@ -1,6 +1,7 @@
 package ru.skillbranch.kotlinexample
 
 import android.support.annotation.VisibleForTesting
+import java.util.function.Consumer
 
 object UserHolder {
     private val map = mutableMapOf<String, User>()
@@ -10,13 +11,49 @@ object UserHolder {
             email: String,
             password: String
     ): User = User.makeUser(fullName, email = email, password = password)
-            .also { user -> map[user.login] = user }
+            .also {
+                if (!map.containsKey(it.login))
+                    map[it.login] = it
+                else
+                    throw IllegalArgumentException("A user with this email already exists")
+            }
+
+    fun registerUserByPhone(
+            fullName: String,
+            rawPhone: String
+    ): User = when {
+        validatePhone(rawPhone) -> User.makeUser(fullName, phone = rawPhone)
+                .also {
+                    if (!map.containsKey(it.login))
+                        map[it.login] = it
+                    else
+                        throw IllegalArgumentException("A user with this phone already exists")
+                }
+        else -> throw IllegalArgumentException("Enter a valid phone number starting with a + and containing 11 digits")
+    }
 
     fun loginUser(login: String, password: String): String? =
-            map[login.trim()]?.let {
+            map[login.replace("[^+\\d]".toRegex(), "")]?.let {
                 if (it.checkPassword(password)) it.userInfo
                 else null
             }
+
+    fun requestAccessCode(login: String): Unit {
+        map[login.replace("[^+\\d]".toRegex(), "")]?.let {
+            it.changePassword(it.accessCode!!, it.generateAccessCode())
+            it.sendAccessCodeToUser(login, it.accessCode!!)
+        }
+    }
+
+    fun importUser(list: List<String>): List<User> {
+        var userList = ArrayList<User>()
+        list.forEach { row -> userList.add(User.importUser(row)) }
+        return userList
+    }
+
+    fun validatePhone(rawPhone: String): Boolean =
+        rawPhone.startsWith("+") && !rawPhone.contains("[А-яа-яA-za-z]".toRegex()) && rawPhone.replace("[^0-9]".toRegex(), "").length == 11
+
 
     @VisibleForTesting(otherwise = VisibleForTesting.NONE)
     fun clearHolder() {
